a9d1968ed9364250ec5ee080f164b853e74ae95f
JohnyBoardProfi <91531366+JohnyBoardProfi@users.noreply.github.com>
2022-02-14 18:30:23 +0200

    Create DoubleFactorial.cs

---
+++ DoubleFactorial.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace DoubleFactorial
+{
+    class Program
+    {
+        static double DF(double n) => n % 2 == 0 ? Math.Pow(2, n / 2) * F(n / 2) : F(n) / (Math.Pow(2, (n - 1) / 2) * F((n - 1) / 2));
+
+        static double F(double n) => n == 1 ? 1 : n * F(n - 1);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            double num = Convert.ToUInt64(Console.ReadLine());
+            Console.WriteLine(DF(num));
+            Console.ReadKey();
+        }
+    }
