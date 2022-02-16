052ff40f6fa4098be0d650eebb79b047c61665a8
JohnyBoardProfi <91531366+JohnyBoardProfi@users.noreply.github.com>
2022-02-16 19:37:59 +0200

    Update Ackermann.cs

--- Ackermann.cs
+++ Ackermann.cs
@@ -4,13 +4,13 @@ namespace Ackermann // Ackermann function
 {
     class Program
     {
-        static int A(uint m, n) => m == 0 ? n + 1 : (n == 0 ? A(m - 1, 1) : A(m - 1, A(m, n - 1)));
+        static int A(uint m, uint n) => m == 0 ? n + 1 : (n == 0 ? A(m - 1, 1) : A(m - 1, A(m, n - 1)));
         
         static void Main(string[] args)
         {
             Console.WriteLine();
             uint m = Convert.ToUInt32(Console.ReadLine()), n = Convert.ToUInt32(Console.ReadLine());
-            Console.WriteLine(SumOfSquares(num));
+            Console.WriteLine(A(m, n));
             Console.ReadKey();
         }
     }
